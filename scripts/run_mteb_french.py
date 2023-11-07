a70a87d3aa09b8057f6e91d8f5531d1115d9eb06
Gabriel Sequeira <gsequeira@openstudio.fr>
2023-11-07 09:27:29 +0100

    create french script eval

--- scripts/run_mteb_french.py
+++ scripts/run_mteb_french.py
@@ -1,12 +1,9 @@
-"""Example script for benchmarking all datasets constituting the MTEB French leaderboard & average scores"""
-
-from __future__ import annotations
+"""Example script for benchmarking all datasets constituting the MTEB English leaderboard & average scores"""
 
 import logging
 
-from sentence_transformers import SentenceTransformer
-
 from mteb import MTEB
+from sentence_transformers import SentenceTransformer
 
 logging.basicConfig(level=logging.INFO)
 
@@ -14,7 +11,7 @@ logger = logging.getLogger("main")
 
 TASK_LIST_CLASSIFICATION = [
     "AmazonReviewsClassification",
-    "MasakhaNEWSClassification",
+    "MasakhaneClassification",
     "MassiveIntentClassification",
     "MassiveScenarioClassification",
     "MTOPDomainClassification",
@@ -22,31 +19,19 @@ TASK_LIST_CLASSIFICATION = [
 ]
 
 TASK_LIST_CLUSTERING = [
-    "AlloProfClusteringP2P",
-    "AlloProfClusteringS2S",
-    "HALClusteringS2S",
-    "MasakhaNEWSClusteringP2P",
-    "MasakhaNEWSClusteringS2S",
-    "MLSUMClusteringP2P",
-    "MLSUMClusteringS2S",
 ]
 
 TASK_LIST_PAIR_CLASSIFICATION = [
-    "OpusparcusPC",
-    "PawsX",
 ]
 
-TASK_LIST_RERANKING = ["SyntecReranking", "AlloprofReranking"]
+TASK_LIST_RERANKING = [
+]
 
 TASK_LIST_RETRIEVAL = [
-    "AlloprofRetrieval",
-    "BSARDRetrieval",
-    "SyntecRetrieval",
-    "XPQARetrieval",
-    "MintakaRetrieval",
 ]
 
-TASK_LIST_STS = ["SummEvalFr", "STSBenchmarkMultilingualSTS", "STS22", "SICKFr"]
+TASK_LIST_STS = [
+]
 
 TASK_LIST = (
     TASK_LIST_CLASSIFICATION
@@ -59,11 +44,9 @@ TASK_LIST = (
 
 model_name = "dangvantuan/sentence-camembert-base"
 model = SentenceTransformer(model_name)
-
 logger.info(f"Task list : {TASK_LIST}")
 for task in TASK_LIST:
     logger.info(f"Running task: {task}")
-    evaluation = MTEB(
-        tasks=[task], task_langs=["fr"]
-    )  # Remove "fr" for running all languages
-    evaluation.run(model, output_folder=f"results/{model_name}")
+    eval_splits = ["dev"] if task == "MSMARCO" else ["test"]
+    evaluation = MTEB(tasks=[task], task_langs=["fr"])  # Remove "fr" for running all languages
+    evaluation.run(model, output_folder=f"results/{model_name}", eval_splits=eval_splits)
