abec6f3aa2b468ca39139c8b2a84f440fed4b62b
Sequeira Gabriel <gabriel.sequeira@outlook.fr>
2023-11-10 12:12:26 +0100

    4 pair classification (#10)
    * add Opusparcus dataset
    
    * multilingual usage
    
    * use eval_split of config files
    
    * change eval_split according to data
    
    ---------
    
    Co-authored-by: Gabriel Sequeira <gsequeira@openstudio.fr>

--- mteb/tasks/PairClassification/OpusparcusPC.py
+++ mteb/tasks/PairClassification/OpusparcusPC.py
@@ -1,10 +1,10 @@
-from ...abstasks import AbsTaskPairClassification, MultilingualTask
+from ...abstasks import AbsTaskPairClassification
 import datasets
 
 _LANGUAGES = ["de", "en", "fi", "fr", "ru", "sv"]
 
 
-class OpusparcusPC(AbsTaskPairClassification, MultilingualTask):
+class OpusparcusPC(AbsTaskPairClassification):
     @property
     def description(self):
         return {
@@ -19,6 +19,16 @@ class OpusparcusPC(AbsTaskPairClassification, MultilingualTask):
             "main_score": "ap",
             "revision": "9e9b1f8ef51616073f47f306f7f47dd91663f86a",
         }
+
+    def __init__(self, langs=None, **kwargs):
+        super().__init__(**kwargs)
+        if type(langs) is list:
+            langs = [lang for lang in langs if lang in self.description["eval_langs"]]
+        if langs is not None and len(langs) > 0:
+            self.langs = langs  # TODO: case where user provides langs not in the dataset
+        else:
+            self.langs = self.description["eval_langs"]
+        self.is_multilingual = True
 
     def load_data(self, **kwargs):
         """
