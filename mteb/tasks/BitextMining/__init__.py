8ef13d07d7a1dee3fe5b4c04b1c97d060437faa5
Imene Kerboua <33312980+imenelydiaker@users.noreply.github.com>
2023-11-13 16:33:28 +0100

    DiaBLa and Flores Bitext Mining evaluation (#12)
    * Add DiaBLa dataset for bitext mining
    
    * Add DiaBLa dataset for bitext mining
    
    * deduplicate bitext task
    
    * add Flores
    
    * format files
    
    * add flores to evaluation script
    
    * remove prints
    
    * add revision
    
    ---------
    
    Co-authored-by: Gabriel Sequeira <gsequeira@openstudio.fr>

--- mteb/tasks/BitextMining/__init__.py
+++ mteb/tasks/BitextMining/__init__.py
@@ -1,10 +1,5 @@
-from __future__ import annotations
-
-from .dan.BornholmskBitextMining import *
-from .multilingual.BUCCBitextMining import *
-from .multilingual.DiaBLaBitextMining import *
-from .multilingual.FloresBitextMining import *
-from .multilingual.norwegian_courts_bitext_mining import *
-from .multilingual.NorwegianCourtsBitextMining import *
-from .multilingual.TatoebaBitextMining import *
-from .vie.VieMedEVBitextMining import *
+from .BornholmskBitextMining import *
+from .BUCCBitextMining import *
+from .DiaBLaBitextMining import *
+from .FloresBitextMining import *
+from .TatoebaBitextMining import *
