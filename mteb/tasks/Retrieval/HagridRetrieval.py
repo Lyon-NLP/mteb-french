e01e09bab670e645f65b05fcb88e4b227501f272
mathieu.ciancone@gmail.com <mciancone@openstudio.fr>
2023-11-13 14:47:48 +0100

    adding Hagrid dataset

--- mteb/tasks/Retrieval/HagridRetrieval.py
+++ mteb/tasks/Retrieval/HagridRetrieval.py
@@ -6,14 +6,15 @@ from ...abstasks.AbsTaskRetrieval import AbsTaskRetrieval
 
 
 class HagridRetrieval(AbsTaskRetrieval):
-    _EVAL_SPLITS = ["dev"]
+
+    _EVAL_SPLIT = 'test'
 
     @property
     def description(self):
         return {
-            "name": "HagridRetrieval",
-            "hf_hub_name": "miracl/hagrid",
-            "reference": "https://github.com/project-miracl/hagrid",
+            'name': 'HagridRetrieval',
+            'hf_hub_name': 'miracl/hagrid',
+            'reference': 'https://github.com/project-miracl/hagrid',
             "description": (
                 "HAGRID (Human-in-the-loop Attributable Generative Retrieval for Information-seeking Dataset)"
                 "is a dataset for generative information-seeking scenarios. It consists of queries"
@@ -21,10 +22,9 @@ class HagridRetrieval(AbsTaskRetrieval):
             ),
             "type": "Retrieval",
             "category": "s2p",
-            "eval_splits": self._EVAL_SPLITS,
+            "eval_splits": ["test"],
             "eval_langs": ["en"],
             "main_score": "ndcg_at_10",
-            "revision": "b2a085913606be3c4f2f1a8bff1810e38bade8fa",
         }
 
     def load_data(self, **kwargs):
@@ -34,19 +34,18 @@ class HagridRetrieval(AbsTaskRetrieval):
         if self.data_loaded:
             return
 
-        data = datasets.load_dataset(
-            "miracl/hagrid", split=self._EVAL_SPLITS[0], revision=self.description.get("revision", None)
-        )
+        data = datasets.load_dataset("miracl/hagrid", split="dev")
         proc_data = self.preprocess_data(data)
 
-        self.queries = {self._EVAL_SPLITS[0]: {d["query_id"]: d["query_text"] for d in proc_data}}
-        self.corpus = {self._EVAL_SPLITS[0]: {d["answer_id"]: {"text": d["answer_text"]} for d in proc_data}}
-        self.relevant_docs = {self._EVAL_SPLITS[0]: {d["query_id"]: {d["answer_id"]: 1} for d in proc_data}}
+        self.queries = {self._EVAL_SPLIT: {d["query_id"]: d['query_text'] for d in proc_data}}
+        self.corpus = {self._EVAL_SPLIT: {d['answer_id']: {'text': d['answer_text']} for d in proc_data}}
+        self.relevant_docs = {self._EVAL_SPLIT: {d["query_id"]: {d['answer_id']: 1} for d in proc_data}}
 
         self.data_loaded = True
 
-    def preprocess_data(self, dataset: Dict) -> List[Dict]:
-        """
+    
+    def preprocess_data(self, dataset:Dict) -> List[Dict]:
+        """ 
         Preprocessed the data in a format easirer
         to handle for the loading of queries and corpus
         ------
@@ -65,21 +64,30 @@ class HagridRetrieval(AbsTaskRetrieval):
                         "query_id": str(d["query_id"]),
                         "query_text": d["query"],
                         "answer_id": str(uuid.uuid4()),
-                        "answer_text": best_answer,
+                        "answer_text": best_answer
                     }
                 )
-
+        
         return preprocessed_data
 
-    def get_best_answer(self, data: Dict) -> str:
+
+    def get_best_answer(self, data:Dict) -> str :
         """
         Get the best answer among available answers
-        of a query.
+        of a query. 
         WARNING : May return None if no good answer available
         --------
         PARAMS:
         data: a dict representing one element of the dataset
         """
-        good_answers = [a["answer"] for a in data["answers"] if a["informative"] == 1 and a["attributable"] == 1]
-        # Return 1st one if >=1 good answers else None
-        return good_answers[0] if len(good_answers) > 0 else None
+        good_answers = [
+            a["answer"] for a in data["answers"]\
+            if a["informative"] == 1\
+            and a["attributable"] == 1
+            ]
+        # if no good answer found, return Non
+        if len(good_answers) == 0:
+            return
+        # if 1 or more answers are ok, just pick the first one
+        else:
+            return good_answers[0]
