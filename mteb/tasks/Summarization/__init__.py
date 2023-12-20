b9792919063a154c90e0d56f87d00d5737ce69dc
Marion Schaeffer <92590517+schmarion@users.noreply.github.com>
2023-12-20 15:02:24 +0100

    SummEval translated to french (#32)

--- mteb/tasks/Summarization/__init__.py
+++ mteb/tasks/Summarization/__init__.py
@@ -1,4 +1,2 @@
-from __future__ import annotations
-
-from .eng.SummEvalSummarization import *
-from .fra.SummEvalFrSummarization import *
+from .SummEvalFrSummarization import *
+from .SummEvalSummarization import *
